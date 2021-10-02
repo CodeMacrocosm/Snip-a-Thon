d37ec143480e8122ad96bcdde6598aefb75f8fa0
PRAKASH KUMAR <68660459+prakash-sah-lab@users.noreply.github.com>
2021-10-03 00:45:47 +0530

    Create oddoreven.go

---
+++ Go/oddoreven.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Print("Enter number : ")
+    var n int
+    fmt.Scanln(&n)
+    /*  Conditional Statement if .... else ........     */
+    if(n%2==0){
+        fmt.Println(n,"is Even number")
+    }else{
+        fmt.Println(n,"is Odd number")
+    }
+}
