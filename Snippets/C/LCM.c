83c105040c7ea5ea2a36c2e655c3a64725cff725
gpranaya0416 <gpranaya0416@gmail.com>
2025-10-31 23:07:51 +0100

    Update LCM.c

--- Snippets/C/LCM.c
+++ Snippets/C/LCM.c
@@ -1,10 +1,12 @@
- #include <stdio.h>
+
+
+#include <stdio.h>
 int main() {
     int n1, n2, max;
     printf("Enter two positive integers: ");
     scanf("%d %d", &n1, &n2);
 
-    // maximum number between n1 and n2 is stored in max
+    // maximum number between n1 and n2 is stored in
     max = (n1 > n2) ? n1 : n2;
 
     while (1) {
